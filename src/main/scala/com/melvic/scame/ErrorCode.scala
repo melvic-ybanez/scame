26fd39aa7e03846732492f5b06349d0694529e11
Melvic <melvic.ybanez2014@gmail.com>
2019-10-14 23:40:40 +0800

    Implement error codes

---
+++ src/main/scala/com/melvic/scame/ErrorCode.scala
@@ -0,0 +1,9 @@
+package com.melvic.scame
+
+trait ErrorCode
+
+object ErrorCode {
+  final case class SymbolNotFound(name: String) extends ErrorCode
+  final case class ExprMismatch(expected: Vector[String], got: String) extends ErrorCode
+  final case class SymbolAlreadyExists(name: String) extends ErrorCode
+}
