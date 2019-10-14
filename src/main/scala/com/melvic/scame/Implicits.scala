6f5a5d38db5b9f70539f601fca101bbbae673d8b
Melvic <melvic.ybanez2014@gmail.com>
2019-10-14 23:40:22 +0800

    Add implicits

---
+++ src/main/scala/com/melvic/scame/Implicits.scala
@@ -0,0 +1,28 @@
+package com.melvic.scame
+
+import com.melvic.scame.Eval.EvalResult
+import com.melvic.scame.Expr.{Cons, SList, SNil}
+
+import scala.annotation.tailrec
+
+trait Implicits {
+  implicit class ExprAsResult[E <: Expr](expr: E) {
+    def valid: EvalResult = Right(expr)
+  }
+
+  implicit class ErrorCodeAsResult[E <: ErrorCode](error: E) {
+    def invalid[A]: ErrorOr[A] = Left(error)
+  }
+
+  implicit class SListToScalaList(list: SList) {
+    def asScalaList: List[Expr] = {
+      @tailrec
+      def recurse(sList: SList, acc: List[Expr]): List[Expr] = sList match {
+        case SNil => acc
+        case Cons(head, tail) => recurse(tail, head :: acc)
+      }
+
+      recurse(list, Nil)
+    }
+  }
+}
