e2f50a5952b52bf56cb88b0d0ebf2ef0068fd262
Melvic <melvic.ybanez2014@gmail.com>
2019-10-16 02:26:08 +0800

    Use ZIO for the ENV

--- src/main/scala/com/melvic/scame/Env.scala
+++ src/main/scala/com/melvic/scame/Env.scala
@@ -1,12 +1,46 @@
 package com.melvic.scame
 
-import com.melvic.scame.ErrorCode.SymbolAlreadyExists
+import com.melvic.scame.ErrorCode.{SymbolAlreadyExists, SymbolNotFound}
+import zio.ZIO
 
-final case class Env(lookup: Map[String, Expr], parent: Env) {
-  def apply(name: String): Option[Expr] = localSearch(name).orElse(parent(name))
+sealed trait Env
 
-  def localSearch(name: String): Option[Expr] = lookup.get(name)
+object Env {
+  type EnvConfig = (String, Env)
+  type ReadEnv = ZIO[EnvConfig, SymbolNotFound, Expr]
 
-  def +(name: String, expr: Expr): ErrorOr[Env] =
-    localSearch(name).map(_ => SymbolAlreadyExists(name)).toLeft(this)
+  case object EmptyEnv extends Env
+
+  final case class NonEmptyEnv(lookup: Map[String, Expr], parent: Env) extends Env
+
+  def register(expr: Expr): ZIO[EnvConfig, SymbolAlreadyExists, Env] = {
+    val add: ZIO[EnvConfig, Expr, Env] = for {
+      _ <- localSearch.flip
+      env <- ZIO.fromFunction[EnvConfig, Env] {
+        case (name, EmptyEnv) => NonEmptyEnv(Map(name -> expr), EmptyEnv)
+        case (name, e @ NonEmptyEnv(lookup, _)) => e.copy(lookup = lookup + (name -> expr))
+      }
+    } yield env
+
+    add.orElse {
+      for {
+        env <- ZIO.environment[EnvConfig]
+        err <- ZIO.fromEither(Left(SymbolAlreadyExists(env._1)))
+      } yield err
+    }
+  }
+
+  def localSearch: ReadEnv = ZIO.fromFunctionM {
+    case (name, EmptyEnv) => ZIO.fail(SymbolNotFound(name))
+    case (name, NonEmptyEnv(lookup, _)) =>
+      ZIO.fromEither(lookup.get(name).toRight(SymbolNotFound(name)))
+  }
+
+  def globalSearch: ReadEnv = for {
+    _ <- localSearch
+    env <- ZIO.access[EnvConfig] {
+      case (name, NonEmptyEnv(_, parent)) => (name, parent)
+    }
+    global <- localSearch.provide(env)
+  } yield global
 }
